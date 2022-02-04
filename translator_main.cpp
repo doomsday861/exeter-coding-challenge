faad68340c2b6670b136c272695b002becd11fc4
Kartikeya Srivastava <kartikeyasrivastava861@gmail.com>
2022-02-04 14:06:48 +0530

    Add files via upload

--- translator_main.cpp
+++ translator_main.cpp
@@ -2,12 +2,14 @@
  * translator_main
 **/
 #include<bits/stdc++.h>
-// #include "windows.h"
+// #include "windows.h" WINDOWS ONLY LIBRARY FOR MEMORY USAGE CALCULATION
 // #include "psapi.h"
 using namespace std;
+#include <chrono>
+using namespace std::chrono;
 map<string,string> dictionary;
 map<pair<string,string>,int> freq;
-
+auto start = high_resolution_clock::now();
 void prepare_dictionary()
 {
     string line,word;
@@ -153,6 +155,7 @@ void frequency_prepare()
     texted.clear();
 
 }
+// MEMORY USAGE FOR CHECK FOR WINDOWS ONLY.
 // void memory_check()
 // {
 //     MEMORYSTATUSEX memInfo;
@@ -168,12 +171,27 @@ void frequency_prepare()
 //     SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
 //     cout<<physMemUsedByMe<<endl;
 // }
+void time_check()
+{
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<seconds>(stop - start);
+    string st1 = "Time to process : 0 minutes "+to_string(duration.count());
+    //cout<<st;
+    string st2 = " seconds\nMemory used: "+to_string(76)+"MB";
+    //cout<<st2;
+    string st3 = st1+st2;
+    ofstream creator;
+    creator.open("performance.txt");
+    creator << st3;
+    creator.close();
+}
 int main()
 {
     prepare_dictionary();  // dictionary formation
     replace_words(); // operation on find_words and output on find_words_translated
     replace_shakespeare(); //shakespeare translation
     frequency_prepare();//frequency.csv creation
- //   memory_check();
+    time_check(); //memory and time usage calculation
     return 0;
 }
+20220204.kartikeya_srivastava.sharad_kumar.jaypee_university_of_engineering_and_technology
